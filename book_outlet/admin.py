71697fd8702f42ed992cc2cf8a789f89d42e250a
ociel <ocielgonzalezsolis@hotmail.com>
2022-07-27 21:58:49 -0800

    last changes

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title", "rating")}
+    list_filter = ("author", "rating")
+    list_display = ("title", "author", "rating", "isBestSelling")
+
+
+admin.site.register(Book, BookAdmin)

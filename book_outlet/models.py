b0ddf3237c436b087fd5b5e979c0d4c9fea0a045
ociel <ocielgonzalezsolis@hotmail.com>
2022-08-02 10:33:04 -0800

    added models, admin panel and relatinships

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,3 +1,4 @@
+from sre_parse import Verbose
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
@@ -6,13 +7,51 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code} , {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(
+        Address, on_delete=models.CASCADE, null=True, related_name="direction")
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True)
     isBestSelling = models.BooleanField(default=False)
     slug = models.SlugField(default='', blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
